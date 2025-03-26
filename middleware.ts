5941ccab6275c823e469abaa4528613abe42a3a7
Fabrice Niyongabo <ninga.fab@gmail.com>
2025-03-26 23:19:12 +0200

    logging authdata within middleware file

--- middleware.ts
+++ middleware.ts
@@ -5,14 +5,17 @@ const protectedRoutes = ["/dashboard", "/profile"];
 const authPageRoutes = ["/login"];
 const apiAuthPrefix = "/api/auth";
 
-export default auth((req) => {
+export default auth(async (req) => {
   const { nextUrl } = req;
+  const authData = await req.auth;
   const isLoggedIn = !!req.auth;
 
   const path = nextUrl.pathname;
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isProtectedRoute = protectedRoutes.includes(path);
   const isAuthPageRoute = authPageRoutes.includes(path);
+
+  console.log({ authData });
 
   if (isApiAuthRoute) {
     return NextResponse.next();
